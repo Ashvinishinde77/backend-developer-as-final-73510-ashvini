e630bc5b48ac590abb0e3e23d66c061d037eae03
Ashvinishinde77 <159464175+Ashvinishinde77@users.noreply.github.com>
2026-08-30 18:15:17 +0530

    Create User.java

---
+++ User.java
@@ -0,0 +1,20 @@
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    // getters and setters
+}
