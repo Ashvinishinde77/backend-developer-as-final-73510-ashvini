7c16336cc1e3e1e108038d0cbca643ab2bdd0d89
Ashvinishinde77 <159464175+Ashvinishinde77@users.noreply.github.com>
2026-08-30 18:18:24 +0530

    Create ReservationRequest.java

---
+++ ReservationRequest.java
@@ -0,0 +1,19 @@
+public class ReservationRequest {
+
+    @NotNull(message = "Resource ID is required")
+    private Long resourceId;
+
+    @NotNull
+    @Positive(message = "Price must be greater than zero")
+    private BigDecimal price;
+
+    @NotNull
+    private LocalDateTime startTime;
+
+    @NotNull
+    private LocalDateTime endTime;
+
+    private ReservationStatus status;
+
+    // getters and setters
+}
