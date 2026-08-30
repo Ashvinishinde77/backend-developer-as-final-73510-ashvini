dfe0b1316dad7a2484074dce1011487545c4ff45
Ashvinishinde77 <159464175+Ashvinishinde77@users.noreply.github.com>
2026-08-30 18:40:34 +0530

    Update LoginRequest.java

--- LoginRequest.java
+++ LoginRequest.java
@@ -1,10 +1,28 @@
+package com.booking.dto;
+
+import jakarta.validation.constraints.NotBlank;
+
 public class LoginRequest {
 
-    @NotBlank
+    @NotBlank(message = "Username is required")
     private String username;
 
-    @NotBlank
+    @NotBlank(message = "Password is required")
     private String password;
 
-    // getters and setters
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
