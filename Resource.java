f25a02fa72624919b81ce9375f538d88193e772f
Ashvinishinde77 <159464175+Ashvinishinde77@users.noreply.github.com>
2026-08-30 18:15:57 +0530

    Create Resource.java

---
+++ Resource.java
@@ -0,0 +1,18 @@
+@Entity
+@Table(name = "resources")
+public class Resource {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String description;
+
+    @Column(nullable = false)
+    private boolean available = true;
+
+    // getters and setters
+}
